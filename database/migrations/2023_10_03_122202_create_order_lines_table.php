45784321390647c56506b2a235f6670d55bca455
dilanelefa <lefakongdilane@gmail.com>
2023-10-03 14:17:20 +0100

    database migrations

---
+++ database/migrations/2023_10_03_122202_create_order_lines_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use App\Models\Order;
+use App\Models\Product;
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('order_lines', function (Blueprint $table) {
+            $table->id();
+            $table->Interger("quantity");
+            $table->foreignIdFor(Product::class)->constrained()->cascadeOnDelete();
+            $table->foreignIdFor(Order::class)->constrained()->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('order_lines');
+    }
+};
